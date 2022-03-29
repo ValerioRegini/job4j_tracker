3f574df1ca60b37cbd6702e91a45cd8c4191a68e
ValerioRegini <6963119@gmail.com>
2022-03-29 16:58:13 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,36 @@
+package ru.job4j.oop;
+
+import java.sql.SQLOutput;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error test = new Error();
+        Error test1 = new Error(true, 123, "Text1");
+        Error test2 = new Error(false, 456, "Text2");
+        Error test3 = new Error(true, 789, "Text3");
+        test.printInfo();
+        test1.printInfo();
+        test2.printInfo();
+        test3.printInfo();
+    }
+}
